e35e678277e9a06db20fd6650f87e42d89b457f0
Setiawan <uyab.elek878@gmail.com>
2022-06-03 02:29:34 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import SatwaList from "./components/satwa/SatwaList";
+import AddSatwa from "./components/satwa/AddSatwa";
+import EditSatwa from "./components/satwa/EditSatwa";
+import SatwaDonasiList from "./components/satwa/SatwaDonasiList";
+import AddSatwaDonasi from "./components/satwa/AddSatwaDonasi";
+import SatwaGambarList from "./components/satwa/SatwaGambarList";
+import AddSatwaGambar from "./components/satwa/AddSatwaGambar";
+import DonasiList from "./components/donasi/DonasiList";
+import AddDonasi from "./components/donasi/AddDonasi";
+import EditDonasi from "./components/donasi/EditDonasi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<><Navbar /><Dashboard /></>} />
+        <Route path="/satwa" element={<><Navbar /><SatwaList /></>} />
+        <Route path="/satwa/tambah" element={<><Navbar /><AddSatwa /></>} />
+        <Route path="/satwa/edit/:id" element={<><Navbar /><EditSatwa /></>} />
+        <Route path="/satwa/donasi/:id" element={<><Navbar /><SatwaDonasiList /></>} />
+        <Route path="/satwa/donasi/tambah/:id" element={<><Navbar /><AddSatwaDonasi /></>} />
+        <Route path="/satwa/gambar/:id" element={<><Navbar /><SatwaGambarList /></>} />
+        <Route path="/satwa/gambar/tambah/:id" element={<><Navbar /><AddSatwaGambar /></>} />
+        <Route path="/donasi" element={<><Navbar /><DonasiList /></>} />
+        <Route path="/donasi/tambah" element={<><Navbar /><AddDonasi /></>} />
+        <Route path="/donasi/edit/:id" element={<><Navbar /><EditDonasi /></>} />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
---
+++ src/components/satwa/AddSatwa.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+const REACT_APP_BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST;
+
+const AddSatwa = () => {
+  const [nama, setNama] = useState('');
+  const [nama_saintifik, setNamaSaintifik] = useState('');
+  const [lokasi, setLokasi] = useState('');
+  let [populasi, setPopulasi] = useState('');
+  const [funfact, setFunfact] = useState('');
+  const [msg, setMsg] = useState('');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    refreshToken();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      await axios.get(`${REACT_APP_BACKEND_HOST}/auth/token`);
+    } catch (error) {
+      if (error.response) {
+        navigate("/");
+      }
+    }
+  }
+
+  const saveSatwa = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${REACT_APP_BACKEND_HOST}/satwa`, {
+        nama,
+        nama_saintifik,
+        lokasi,
+        populasi,
+        funfact
+      });
+      navigate("/satwa");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.message);
+      }
+    }
+  }
+
+  return (
+    <div className="container is-fluid mt-5">
+      <Link to="/satwa" className="button is-default mb-5"><span className="file-icon">
+        <i className="fa fa-arrow-left"></i>
+      </span>Kembali</Link>
+      <div className="columns mt-5 is-centered">
+        <div className="column is-half">
+          <h1 className="title has-text-centered">Data Satwa</h1>
+          <form onSubmit={saveSatwa}>
+            <p className="has-text-centered">{msg}</p>
+            <div className="field">
+              <label className="label">Nama</label>
+              <div className="control">
+                <input type="text" className="input" placeholder="Nama" value={nama} onChange={(e) => setNama(e.target.value)} />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Nama Saintifik (Opsional)</label>
+              <div className="control">
+                <input className="input" placeholder="Nama Saintifik" value={nama_saintifik} onChange={(e) => setNamaSaintifik(e.target.value)}></input>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Lokasi (Opsional)</label>
+              <div className="control">
+                <input type="text" className="input" placeholder="Lokasi" value={lokasi} onChange={(e) => setLokasi(e.target.value)} />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Populasi (Opsional)</label>
+              <div className="control">
+                <input type="text" className="input" placeholder="Populasi" value={populasi} onChange={(e) => setPopulasi(e.target.value)} />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Funfact (Opsional)</label>
+              <div className="control">
+                <textarea type="text" className="textarea" placeholder="Funfact" value={funfact} onChange={(e) => setFunfact(e.target.value)} ></textarea>
+              </div>
+            </div>
+            <div className="field">
+              <button type="submit" className="button is-success"><span className="file-icon">
+                <i className="fa fa-check"></i>
+              </span>Simpan</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddSatwa
